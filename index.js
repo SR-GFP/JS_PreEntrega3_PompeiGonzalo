77ceb5a71dec871ba664f018fa3068f1d5f2a603
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-03-29 01:07:20 -0300

    1Commit Rama1: se crea funcion para vincular nodos para ingreso de ususario

--- index.js
+++ index.js
@@ -4,15 +4,23 @@
     Nota: las variables y los nodos deben tener el mismo nombre de ID*/
 
 const listaDeVariablesParaNodos = [
-    botonAdministrador,
-    botonUsuario,
-    prueba,    
+    btnLogin,
+    ventanaEmergente,
+    inputUsuario,
+    inputPassword,
+    inputIngresar,
+    btnCerrar,
 ]
-function vincularNodos(array) {
-    for (let i = 0; i < array.length; i++) {
-        let i = document.querySelector("#i")
-    }
+const vincularNodos =(array) => {    
+        let i = document.querySelector("#i")    
 }
 listaDeVariablesParaNodos.forEach(vincularNodos)
 
-/*  */
+/*Log in*/
+btnLogin.onclick = function() {
+    ventanaEmergente.className = "ver";
+}
+
+btnCerrar.onclick = function (){
+    ventanaEmergente.style.display = "none";
+}
