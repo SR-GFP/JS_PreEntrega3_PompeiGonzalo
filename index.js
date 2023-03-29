b04c2af1a0a12b3a80fc7549d1290c37ef45cfb5
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-03-28 23:24:42 -0300

    commit master seguimiento de archivos

---
+++ index.js
@@ -0,0 +1,18 @@
+//----------VINCULACION DE NODOS-----------////
+/*  Function abstracta para vinvulacion de nodos
+    Solo se deben agregar al array los nombres de las variables a vincular con los Nodos
+    Nota: las variables y los nodos deben tener el mismo nombre de ID*/
+
+const listaDeVariablesParaNodos = [
+    botonAdministrador,
+    botonUsuario,
+    prueba,    
+]
+function vincularNodos(array) {
+    for (let i = 0; i < array.length; i++) {
+        let i = document.querySelector("#i")
+    }
+}
+listaDeVariablesParaNodos.forEach(vincularNodos)
+
+/*  */
